8d34c9631b2914943b5ce11b17f2d24701280bee
Thong1901 <thongthanhmai129@gmail.com>
2025-05-14 01:06:30 +0700

    fix

--- FE_QLVC/src/main.jsx
+++ FE_QLVC/src/main.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { AuthProvider } from "./contexts/AuthContext";
-import "./assets/index.css";
-import AntdConfigProvider from "./utils/antdConfigProvider";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { AuthProvider } from './contexts/AuthContext';
+import './assets/index.css';
+import AntdConfigProvider from './utils/antdConfigProvider';
 
 // Initialize Ant Design config to suppress warnings
-import "./utils/antdConfigProvider";
+import './utils/antdConfigProvider';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  // <React.StrictMode>
-  // </React.StrictMode>,
-  <AuthProvider>
-    <AntdConfigProvider>
-      <App />
-    </AntdConfigProvider>
-  </AuthProvider>
-);
+  <React.StrictMode>
+    <AuthProvider>
+      <AntdConfigProvider>
+        <App />
+      </AntdConfigProvider>
+    </AuthProvider>
+  </React.StrictMode>,
+)
